5e080fe1479738cd4ee3b76ebcb4c582dbf18e39
Anton <ant.filatov@gmail.com>
2019-12-23 16:01:50 +0300

    deleted generated artifacts

--- ros/catkin_ws/build/duckietown_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/anton/github/aido-auto/ros/catkin_ws/src/duckietown_msgs
-# Build directory: /home/anton/github/aido-auto/ros/catkin_ws/build/duckietown_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ros/catkin_ws/build/duckietown_msgs/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/anton/github/aido-auto/ros/catkin_ws/install/include".split(';') if "/home/anton/github/aido-auto/ros/catkin_ws/install/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "geometry_msgs;roscpp;rospy;sensor_msgs;std_msgs;message_runtime;visualization_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "duckietown_msgs"
-PROJECT_SPACE_DIR = "/home/anton/github/aido-auto/ros/catkin_ws/install"
-PROJECT_VERSION = "0.0.0"
